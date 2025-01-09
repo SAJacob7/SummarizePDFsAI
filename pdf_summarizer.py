6e0e6990769abc1b70bc50b68c0a1fc4770b8a37
SAJacob7 <SAJacob77@gmail.com>
2025-01-09 14:15:11 -0600

    Commit pdf summarized, but not well

--- pdf_summarizer.py
+++ pdf_summarizer.py
@@ -1,5 +1,7 @@
 import re
-from youtube_transcript_api import YouTubeTranscriptApi
+import PyPDF2
+import pdfplumber
+import requests
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
@@ -11,29 +13,21 @@ import heapq
 # nltk.download('punkt_tab')
 
 # The first step is to get the URL and its id to get the transcript.
-def get_id_of_video(url_link):
-    video_id_regex = r'(?:v=|\/)([0-9A-Za-z_-]{11}).*'
-    match = re.search(video_id_regex, url_link)
+def get_pdf_from_url(url_link, file_path):
+    response = requests.get(url_link)
+    with open(file_path, 'wb') as file:
+        file.write(response.content)
+def get_text_from_pdf(file_path):
     try:
-        if match:
-            return match.group(1)
-        else:
-            raise KeyError
+        with pdfplumber.open(file_path) as pdf:
+            text = ''
+            for page in pdf.pages:
+                text += page.extract_text()
+        return text
     except:
-        print("Error in getting ID")
-        return None
-def get_transcript_of_video(url_id):
-    try:
-        transcript = YouTubeTranscriptApi.get_transcript(url_id)
-        return transcript
-    except:
-        print("Error in getting transcript")
+        print("Error in getting text from PDF")
         return None
 # Now that we got the id and the transcript, the second step is to summarize it.
-def transcript_to_text(transcript):
-    text_list = [transcript[i]['text'] for i in range(len(transcript))]
-    transcript_text = '\n'.join(text_list)
-    return transcript_text
 def summarize_text(text):
     stopwords = nltk.corpus.stopwords.words('english')
     sentence_list = text.split("\n")
@@ -61,17 +55,15 @@ def summarize_text(text):
                     sentence_frequency[sentence] = word_frequency[word]
                 else:
                     sentence_frequency[sentence] += word_frequency[word]
-    summary = heapq.nlargest(20, sentence_frequency, key=sentence_frequency.get)
+    summary = heapq.nlargest(30, sentence_frequency, key=sentence_frequency.get)
     return summary
 def main():
-    url_link = 'https://www.youtube.com/watch?v=H14bBuluwB8'
-    url_id = get_id_of_video(url_link)
-    if url_id != None:
-        transcript = get_transcript_of_video(url_id)
-        if transcript != None:
-            text = transcript_to_text(transcript)
-            summary = summarize_text(text)
-            print("Summary: ")
-            for i in summary:
-                print(i)
+    url_link = 'https://www.whitehouse.gov/wp-content/uploads/2022/12/TTC-EC-CEA-AI-Report-12052022-1.pdf'
+    get_pdf_from_url(url_link, '/Users/sophiajacob/Downloads/SummarizePDFProject/file.pdf')
+    text = get_text_from_pdf('/Users/sophiajacob/Downloads/SummarizePDFProject/file.pdf')
+    if text != None:
+        summary = summarize_text(text)
+        print("Summary: ")
+        for i in summary:
+            print(i)
 main()
