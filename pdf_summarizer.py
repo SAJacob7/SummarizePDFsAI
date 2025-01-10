c4ffe94440b556b16925ecc941ec35a8c2030f3c
SAJacob7 <SAJacob77@gmail.com>
2025-01-09 21:46:31 -0600

    Commit a better pdf summarizer with an in-built algorithm and one that I made using TF-IDF

--- pdf_summarizer.py
+++ pdf_summarizer.py
@@ -5,7 +5,11 @@ import requests
 import nltk
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.stem.snowball import SnowballStemmer
 import heapq
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 
 # Download required NLTK resources. This only needs to be done once.
 # nltk.download('punkt')
@@ -31,16 +35,17 @@ def get_text_from_pdf(file_path):
 def summarize_text(text):
     stopwords = nltk.corpus.stopwords.words('english')
     sentence_list = text.split("\n")
+    stemmer = SnowballStemmer("english", ignore_stopwords=True)
     # print(sentence_list)
     # Frequency of words to scores dictionary.
     word_frequency = {}
     word_list = nltk.word_tokenize(text) # Tokenize based on each word.
     for word in word_list:
         if word not in stopwords:
-            if word not in word_frequency:
-                word_frequency[word] = 1
+            if stemmer.stem(word) not in word_frequency:
+                word_frequency[stemmer.stem(word)] = 1
             else:
-                word_frequency[word] += 1
+                word_frequency[stemmer.stem(word)] += 1
     max_freq = max(word_frequency.values())
     # print(word_frequency)
     for word in word_frequency:
@@ -50,20 +55,34 @@ def summarize_text(text):
     sentence_frequency = {}
     for sentence in sentence_list:
         for word in word_list:
-            if word in word_frequency and len(sentence.split(' ')) < 35 and word in sentence:
+            if stemmer.stem(word) in word_frequency and word in sentence:
                 if sentence not in sentence_frequency:
-                    sentence_frequency[sentence] = word_frequency[word]
+                    sentence_frequency[sentence] = word_frequency[stemmer.stem(word)]
                 else:
-                    sentence_frequency[sentence] += word_frequency[word]
-    summary = heapq.nlargest(30, sentence_frequency, key=sentence_frequency.get)
+                    sentence_frequency[sentence] += word_frequency[stemmer.stem(word)]
+    summary_length = int(len(sentence_frequency) * 0.2)
+    summary = heapq.nlargest(summary_length, sentence_frequency, key=sentence_frequency.get)
+    return summary
+def summary_text2(text):
+    sentence_list = text.split("\n")
+    sentences_count = int(len(sentence_list) * 0.2)
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+    summary = summarizer(parser.document, sentences_count)
     return summary
 def main():
-    url_link = 'https://www.whitehouse.gov/wp-content/uploads/2022/12/TTC-EC-CEA-AI-Report-12052022-1.pdf'
+    url_link = 'https://stmarysguntur.com/wp-content/uploads/2019/07/UNIT-1-converted-converted.pdf'
     get_pdf_from_url(url_link, '/Users/sophiajacob/Downloads/SummarizePDFProject/file.pdf')
     text = get_text_from_pdf('/Users/sophiajacob/Downloads/SummarizePDFProject/file.pdf')
     if text != None:
         summary = summarize_text(text)
         print("Summary: ")
+        for i in summary:
+            print(i)
+        summary = summary_text2(text)
+        print()
+        print()
+        print("Second Summary: ")
         for i in summary:
             print(i)
 main()
